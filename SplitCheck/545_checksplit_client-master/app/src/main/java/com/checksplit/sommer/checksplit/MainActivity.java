69c854fe95684bbd872cf6fdbfe8077106479c12
Brandon Patton <bpatton@stevens.edu>
2020-05-22 18:08:57 -0400

    CS 545 Coursework

---
+++ SplitCheck/545_checksplit_client-master/app/src/main/java/com/checksplit/sommer/checksplit/MainActivity.java
@@ -0,0 +1,49 @@
+package com.checksplit.sommer.checksplit;
+
+
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.checksplit.sommer.checksplit.Utils.NavigationUtils;
+import com.checksplit.sommer.checksplit.databinding.ActivityMainBinding;
+import com.checksplit.sommer.checksplit.selectItemsActivity.SelectItemsActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+public class MainActivity extends AppCompatActivity {
+    QRScanFragment qrScanFragment;
+    HomeButtonsFragment homeButtonsFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Set up binding for MainActivity
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setLifecycleOwner(this);
+
+        homeButtonsFragment = new HomeButtonsFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer, homeButtonsFragment).commit();
+
+        qrScanFragment = new QRScanFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainerQRScan, qrScanFragment).commit();
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        n();
+                    }
+                });
+            }
+        },1000);
+    }
+
+    private void n(){
+        NavigationUtils.presentActivity(this, SelectItemsActivity.class);
+    }
+}
